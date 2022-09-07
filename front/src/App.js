cad4de7899a872e8065b5b3b2fc77e7449de175c
Mrkriptio <konstantinzupa@gmail.com>
2022-09-07 09:07:47 +0300

    busket almost done + styles + cards done

---
+++ front/src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+
+function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://630e591337925634187bff3c.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
+  const onAddToCart = (obj) => {
+    console.log(obj)
+    setCartItems((prev) => [...prev, obj]);
+  };
+
+  const onChangeSearchInput = (event) => {
+    console.log(event.target.value)
+    //setSearchValue();
+  }
+
+  return (
+    <div className="wrapper clear">
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
+      <Header onClickCart={() => setCartOpened(true)} />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input onChange={onChangeSearchInput}
+                   placeholder="Поиск..." />
+          </div>
+        </div>
+
+        <div className="d-flex flex-wrap">
+          {items.map((item,index) => (
+            <Card
+                key={index}
+              title={item.title}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              onFavorite={() => console.log('Добавили в закладки')}
+              onPlus={(obj) => onAddToCart(obj)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
