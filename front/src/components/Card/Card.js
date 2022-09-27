865fc8932cdf953728979b01e242deffa774c8c0
Kriptio-root <konstantinzupa@gmail.com>
2022-09-27 23:22:18 +0300

    fixes

--- front/src/components/Card/Card.js
+++ front/src/components/Card/Card.js
@@ -1,42 +1,67 @@
 import React from 'react';
+import ContentLoader from "react-content-loader"
+
 import './Card.scss'
 
-function Card({ title, imageUrl, price, onFavorite, onPlus,id,favorited=false }) {
-    const [isAdded,setIsAdded] = React.useState(false);
-    const [isFavorite,setIsFavorite] = React.useState(favorited);
-    const imgRegex=".jpg"
+function Card({title, imageUrl, price, onFavorite, onPlus, id, favorited = false, added = false, loading = false}) {
+    const [isAdded, setIsAdded] = React.useState(added);
+    const [isFavorite, setIsFavorite] = React.useState(favorited);
+    const imgRegex = ".jpg"
 
     const onClickPlus = () => {
-        setIsAdded(isAdded=>!isAdded)
-        onPlus({ title, imageUrl, price,isAdded,id });
+        setIsAdded(isAdded => !isAdded)
+        onPlus({title, imageUrl, price, isAdded, id});
     };
     const onClickFavorite = () => {
         setIsFavorite(!isFavorite)
-        onFavorite({ title, imageUrl, price,isAdded,id });
+        onFavorite({title, imageUrl, price, isAdded, id});
     }
 
 
-  return (
-    <div className="card">
-      <div className="favorite" onClick={onClickFavorite}>
-        <img src={isFavorite ? '/img/heart-liked.svg' : "/img/heart-unliked.svg"} alt="Unliked" />
-      </div>
-      <img width={133} height={112} src={[imageUrl.slice(0,imageUrl.search(imgRegex)),id, imageUrl.slice(imageUrl.search(imgRegex))].join('')} alt={imageUrl.toString()}/>
-      <h5>{title}</h5>
-      <div className="d-flex justify-between align-center">
-        <div className="d-flex flex-column">
-          <span>Price:</span>
-          <b>{price}</b>
+    return (
+        <div className="card">
+            {loading ?
+                (
+                    <ContentLoader
+                        speed={2}
+                        width={155}
+                        height={250}
+                        viewBox="0 0 155 265"
+                        backgroundColor="#f3f3f3"
+                        foregroundColor="#ecebeb">
+                        <rect x="1" y="0" rx="10" ry="10" width="155" height="155"/>
+                        <rect x="0" y="167" rx="5" ry="5" width="155" height="15"/>
+                        <rect x="0" y="187" rx="5" ry="5" width="100" height="15"/>
+                        <rect x="1" y="234" rx="5" ry="5" width="80" height="25"/>
+                        <rect x="124" y="230" rx="10" ry="10" width="32" height="32"/>
+                    </ContentLoader>
+                )
+                :
+                <>
+                    <div className="favorite" onClick={onClickFavorite}>
+                        <img src={isFavorite ? '/img/heart-liked.svg' : "/img/heart-unliked.svg"} alt="Unliked"/>
+                    </div>
+                    <img width='100%' height={135}
+                         src={[imageUrl.slice(0, imageUrl.search(imgRegex)), id, imageUrl.slice(imageUrl.search(imgRegex))].join('')}
+                         alt={imageUrl.toString()}/>
+                    <h5>{title}</h5>
+                    <div className="d-flex justify-between align-center">
+                        <div className="d-flex flex-column">
+                            <span>Price:</span>
+                            <b>{price}</b>
+                        </div>
+                        <img
+                            className="plus"
+                            onClick={onClickPlus}
+                            src={isAdded ? '/img/btn-checked.svg' : '/img/btn-plus.svg'}
+                            alt="PlusImg"
+                        />
+                    </div>
+                </>
+
+            }
         </div>
-          <img
-              className="plus"
-              onClick={onClickPlus}
-              src={isAdded ? '/img/btn-checked.svg' : '/img/btn-plus.svg'}
-              alt="PlusImg"
-          />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Card;
--- front/src/pages/home.jsx
+++ front/src/pages/home.jsx
@@ -14,45 +14,79 @@ function Home() {
     const [favorites, setFavorites] = React.useState([]);
     const [cartItems, setCartItems] = React.useState([]);
     const [cartOpened, setCartOpened] = React.useState(false);
+    const [isLoading, setIsLoading] = React.useState(true);
 
     const onChangeSearchInput = (event) => {
         //console.log(event.target.value)
         setSearchValue(event.target.value);
     }
 
-    React.useEffect(() => {
-        axios.get('https://630e591337925634187bff3c.mockapi.io/items')
-            .then(res => {
-                setItems(res.data);
-            });
-        axios.get('https://630e591337925634187bff3c.mockapi.io/cart')
-            .then(res => {
-                setCartItems(res.data);
-            });
-        axios.get('https://630e591337925634187bff3c.mockapi.io/favorites')
-            .then(res => {
-                setFavorites(res.data);
-            });
+    const renderItems =() => {
+        const filteredItems =items.filter((item) => item.title.toString().toLowerCase().includes(searchValue.toString().toLowerCase()))
+    return (
+        isLoading ? [...Array(8)] : filteredItems
+                .map((item, index) => (
+                    <Card
+                    key={index}
+                    title={item.title}
+                    price={item.price}
+                    imageUrl={item.imageUrl}
+                    id={item.id}
+                    onFavorite={(obj) => onAddToFavorite(obj)}
+                    onPlus={(obj) => onAddToCart(obj)}
+                    added={cartItems.some((obj) => Number(obj.id) === Number(item.id))}
+                    loading={isLoading}
+                />))
+    )
+
+    }
+
+    React.useEffect( () => {
+        async function fetchData() {
+            setIsLoading(true)
+
+            const {data:itemsResponse} = await axios.get('https://630e591337925634187bff3c.mockapi.io/items')
+
+            const {data:cartResponse}  = await  axios.get('https://630e591337925634187bff3c.mockapi.io/cart')
+
+            const {data:favoritesResponse}  = await axios.get('https://630e591337925634187bff3c.mockapi.io/favorites')
+
+            setIsLoading(false)
+
+            setCartItems(cartResponse);
+            setFavorites(favoritesResponse);
+            setItems(itemsResponse);
+        }
+        fetchData();
     }, []);
 
     const onAddToFavorite = async (obj) => {
-        try{
-        if (favorites.find(favObj => favObj.id == obj.id)) {
-            axios.delete(`https://630e591337925634187bff3c.mockapi.io/favorites/${obj.id}`)
-            setFavorites((prev) => prev.filter(item => item.id !== obj.id))
-        } else{
-           const {data} = await axios.post('https://630e591337925634187bff3c.mockapi.io/favorites', obj);
-            setFavorites((prev) => [...prev, data]);
-        }
-        }catch (error){
+        try {
+            if (favorites.find(favObj => favObj.id === obj.id)) {
+                axios.delete(`https://630e591337925634187bff3c.mockapi.io/favorites/${obj.id}`)
+                setFavorites((prev) => prev.filter(item => item.id !== obj.id))
+            } else {
+                const {data} = await axios.post('https://630e591337925634187bff3c.mockapi.io/favorites', obj);
+                setFavorites((prev) => [...prev, data]);
+            }
+        } catch (error) {
             alert('cant add to favorites,try it now')
         }
 
     };
 
     const onAddToCart = (obj) => {
-        axios.post('https://630e591337925634187bff3c.mockapi.io/cart', obj);
-        setCartItems((prev) => [...prev, obj]);
+        try {
+            if (cartItems.find((item) => Number(item.id) === Number(obj.id))) {
+                setCartItems(prev => prev.filter(item => Number(item.id) !== Number(obj.id)));
+                axios.delete(`https://630e591337925634187bff3c.mockapi.io/cart/${obj.id}`);
+            } else {
+                axios.post('https://630e591337925634187bff3c.mockapi.io/cart', obj);
+                setCartItems((prev) => [...prev, obj]);
+            }
+        } catch (error) {
+
+        }
     };
 
     const onRemoveItem = (id) => {
@@ -62,44 +96,37 @@ function Home() {
 
     return (
         <div className="wrapper clear">
-                {cartOpened &&
-                    <Drawer
-                        items={cartItems}
-                        onClose={() => setCartOpened(false)}
-                        onRemove={onRemoveItem}/>}
-                <Header onClickCart={() => setCartOpened(true)}/>
+            {cartOpened &&
+                <Drawer
+                    items={cartItems}
+                    onClose={() => setCartOpened(false)}
+                    onRemove={onRemoveItem}/>}
+            <Header onClickCart={() => setCartOpened(true)}/>
             <Routes>
                 <Route path="*" exact element={
                     <div className="content p-40">
-                    <div className="d-flex align-center justify-between mb-40">
-                        <h1>{searchValue.length > 0 && searchValue.toString() !== '' ? `Search for request : "${searchValue}"` : 'All Sneakers'}</h1>
-                        <div className="search-block d-flex">
-                            <img src="/img/search.svg" alt="Search"/>
-                            <input
-                                onChange={onChangeSearchInput}
-                                value={searchValue}
-                                placeholder="Search..."/>
-                            {searchValue &&
-                                <img onClick={() => setSearchValue('')} className="clearImg" src="/img/btn-remove.svg"
-                                     alt="ClearImg"/>}
+                        <div className="d-flex align-center justify-between mb-40">
+                            <h1>{searchValue.length > 0 && searchValue.toString() !== '' ? `Search for request : "${searchValue}"` : 'All Sneakers'}</h1>
+                            <div className="search-block d-flex">
+                                <img src="/img/search.svg" alt="Search"/>
+                                <input
+                                    onChange={onChangeSearchInput}
+                                    value={searchValue}
+                                    placeholder="Search..."/>
+                                {searchValue &&
+                                    <img onClick={() => setSearchValue('')} className="clearImg"
+                                         src="/img/btn-remove.svg"
+                                         alt="ClearImg"/>}
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="d-flex flex-wrap">
-                        {items.filter((item) => item.title.toString().toLowerCase().includes(searchValue.toString().toLowerCase()))
-                            .map((item, index) => (<Card
-                                key={index}
-                                title={item.title}
-                                price={item.price}
-                                imageUrl={item.imageUrl}
-                                id={item.id}
-                                onFavorite={(obj) => onAddToFavorite(obj)}
-                                onPlus={(obj) => onAddToCart(obj)}
-                            />))}
-                    </div>
-                </div>}>
+                        <div className="d-flex flex-wrap">
+                            {renderItems()}
+                        </div>
+                    </div>}>
                 </Route>
-                <Route path='favorites' exact element={<Favorites items={{favorites}} onFavorite={(obj) => onAddToFavorite(obj)}/>}></Route>
+                <Route path='favorites' exact
+                       element={<Favorites items={{favorites}} onFavorite={(obj) => onAddToFavorite(obj)}/>}></Route>
             </Routes>
         </div>
     )
