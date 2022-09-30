7e5acf8a292524af6e4df94db88cbb0925138300
Kriptio-root <konstantinzupa@gmail.com>
2022-09-30 16:37:58 +0300

    search img fix

--- front/src/components/Search/Search.js
+++ front/src/components/Search/Search.js
@@ -3,14 +3,14 @@ function Search({searchValue, setSearchValue, onChangeSearchInput}){
         <div className="d-flex align-center justify-between mb-40">
             <h1>{searchValue.length > 0 && searchValue.toString() !== '' ? `Search for request : "${searchValue}"` : 'All Sneakers'}</h1>
             <div className="search-block d-flex">
-                <img src="/img/search.svg" alt="Search"/>
+                <img src="img/search.svg" alt="Search"/>
                 <input
                     onChange={onChangeSearchInput}
                     value={searchValue}
                     placeholder="Search..."/>
                 {searchValue &&
                     <img onClick={() => setSearchValue('')} className="clearImg"
-                         src="/img/btn-remove.svg"
+                         src="img/btn-remove.svg"
                          alt="ClearImg"/>}
             </div>
         </div>
